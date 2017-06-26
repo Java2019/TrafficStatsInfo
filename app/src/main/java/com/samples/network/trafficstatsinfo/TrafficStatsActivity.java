d05941a43f904fd4580ab865b75ac3490be7aef6
java2019 <java_2019@mail.ru>
2017-06-26 16:07:29 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/network/trafficstatsinfo/TrafficStatsActivity.java
@@ -0,0 +1,33 @@
+package com.samples.network.trafficstatsinfo;
+
+import android.app.Activity;
+import android.net.TrafficStats;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TrafficStatsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_stats);
+
+        final TextView text = (TextView)findViewById(R.id.text);
+
+        text.append("Total:");
+        text.append("\n\tRX Bytes:\t" + TrafficStats.getTotalRxBytes());
+        
+        text.append("\n\tRX Packets:\t" +
+                TrafficStats.getTotalRxPackets());
+        text.append("\n\tTX Bytes:\t" + TrafficStats.getTotalTxBytes());
+        text.append("\n\tTX Packets:\t" +
+                TrafficStats.getTotalTxPackets());
+        text.append("\nMobile:");
+        text.append("\n\tRX Bytes:\t" + TrafficStats.getMobileRxBytes());
+        text.append("\n\tRX Packets:\t" +
+                TrafficStats.getMobileRxPackets());
+        text.append("\n\tTX Bytes:\t" + TrafficStats.getMobileTxBytes());
+        text.append("\n\tTX Packets:\t" +
+                TrafficStats.getMobileTxPackets());
+    }
+}
